dce69c88e1fa6a6456daef9457d079fa1e06dae5
jirgens <jessa.irg@gmail.com>
2014-05-30 14:50:47 -0700

    basic search_by_title

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,20 +1,16 @@
 class VideosController < ApplicationController
-  before_filter :find_video, only: [:show]
 
   def index
     @categories = Category.all.order(:name)
   end
     
   def show
+    @video = Video.find(params[:id])
   end
 
   def search
-    @results = Video.search_by_title(params[:search])
+    @results = Video.search_by_title(params[:search_term])
   end
     
-  private
 
-  def find_video
-    @video = Video.find(params[:id])
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Myflix::Application.routes.draw do
   get "/home", to: "videos#index"
    # root to: "videos#index"
 
-  resources :videos, only: [:show, :index]
+  resources :videos, only: [:show, :index] do
+    collection do
+      post :search, to: "videos#search"
+    end
+  end
   # get 'ui(/:action)', controller: 'ui'
   resources :categories, only: [:show, :index]
 
