08a6e65095e635a2c24437c21690fb7c4d6aaaf9
jirgens <jessa.irg@gmail.com>
2014-06-10 11:51:56 -0700

    added basic rating view passed rspec

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+comedy = Category.create(name: "Comedy")
+drama = Category.create(name: "Drama")
+western = Category.create(name: "Western")
+
 Video.create(
   category: Category.find_by(name: "Comedy"),
   title: "Family Guy",
@@ -19,21 +23,22 @@ Video.create(
   small_cover_url: "/tmp/futurama.jpg",
   large_cover_url: "/tmp/monk_large.jpg")
 
-Video.create(
-  category: Category.find_by(name: "Western"),
-  title: "Monk",
-  description: "Silly OCD PI and his neurotic misadventures.",
-  small_cover_url: "/tmp/monk.jpg",
-  large_cover_url: "/tmp/monk_large.jpg")
+monk = Video.create(title: "Monk", description: "Silly OCD PI and his neurotic misadventures.", category: western, small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg")
+  # category: Category.find_by(name: "Western"),
+  # title: "Monk",
+  # description: "Silly OCD PI and his neurotic misadventures.",
+  # small_cover_url: "/tmp/monk.jpg",
+  # large_cover_url: "/tmp/monk_large.jpg")
 
-Video.create(
-  category: Category.find_by(name: "Comedy"),
-  title: "South Park",
-  description: "Miscirent young boys wild in Colorado.",
-  small_cover_url: "/tmp/south_park.jpg",
-  large_cover_url: "/tmp/monk_large.jpg")
+Video.create(title: "South Park", description: "Miscirent young boys wild in Colorado.", category: comedy, small_cover_url: "/tmp/south_park.jpg", large_cover_url: "/tmp/monk_large.jpg")
+  # category: Category.find_by(name: "Comedy"),
+  # title: "South Park",
+  # description: "Miscirent young boys wild in Colorado.",
+  # small_cover_url: "/tmp/south_park.jpg",
+  # large_cover_url: "/tmp/monk_large.jpg")
   
 
-Category.create(name: "Comedy")
-Category.create(name: "Drama")
-Category.create(name: "Western")
+Joss = User.create(full_name: "Joss Stone", password: "password", email: "joss@stone.com")
+
+Review.create(user: Joss, video: monk, rating: 5, content: "This is the most awesome thing I've ever seen.")
+Review.create(user: Joss, video: monk, rating: 2, content: "I really did not like this at all.")
