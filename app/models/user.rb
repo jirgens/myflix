5142c63fdb3111904e67401632e8de91f81c42b8
jirgens <jessa.irg@gmail.com>
2014-06-27 17:55:11 -0700

    tokenable concern

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  include Tokenable
   validates_presence_of :email, :password, :full_name
   validates_uniqueness_of :email
 
@@ -6,8 +7,6 @@ class User < ActiveRecord::Base
   has_many :reviews, -> { order("created_at DESC")}
   has_many :queue_items, -> { order(:position) }
   has_many :following_relationships, class_name: "Relationship", foreign_key: :follower_id
-
-  before_create :generate_token
 
   def normalize_queue_item_positions
     queue_items.each_with_index do |queue_item, index|
@@ -29,9 +28,5 @@ class User < ActiveRecord::Base
 
   def can_follow?(another_user)
     !(self.follows?(another_user) || self == another_user) 
-  end
-
-  def generate_token
-    self.token = SecureRandom.urlsafe_base64
   end
 end
